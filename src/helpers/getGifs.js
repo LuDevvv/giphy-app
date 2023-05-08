957f4b07d79ae9cb4ce8a60eb7eaba66465d6085
Luis Angel Garcia Louis <ludevgarcialouis@gmail.com>
2023-05-08 16:55:06 -0400

    fix whitespace and add environment variables ✅

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,9 +1,8 @@
 
 export const getGifs = async (category) => {
-  const api_key = import.meta.env.API_KEY;
-  // console.log(api_key)
+  const api_key = import.meta.env.VITE_API_KEY;
 
-  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=20&api_key=L3YCVyEw2AuyiWS7NJlwsXpMROL1jAlR`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=20&api_key=${api_key}`;
 
   const res = await fetch(url);
   const { data } = await res.json();
